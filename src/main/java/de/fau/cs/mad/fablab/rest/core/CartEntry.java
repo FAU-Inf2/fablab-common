b3722c372e01a8a5b8feb41d718ecbb01b271e90
Stefan Herpich <Stefan.Herpich@t-online.de>
2015-05-14 23:23:03 +0200

    removed persistence annotations in CartEntry

--- src/main/java/de/fau/cs/mad/fablab/rest/core/CartEntry.java
+++ src/main/java/de/fau/cs/mad/fablab/rest/core/CartEntry.java
@@ -2,34 +2,15 @@ package de.fau.cs.mad.fablab.rest.core;
 
 
 import java.io.Serializable;
-import javax.persistence.*;
 
 
 /**
  * Created by EE on 12.05.15.
  */
 
-@Entity(name = "cartEntry")
-
 public class CartEntry extends Product implements Serializable{
 
-    @Id
-    private long productId;
-
-    @Column(name = "amount")
     private double amount;
-
-    @Column(name = "categoryString")
-    private String categoryString;
-
-    @Column(name = "price")
-    private double price;
-
-    @Column(name = "categoryId")
-    private long categoryId;
-
-    @Column(name = "name")
-    private String name;
 
 
     public CartEntry(Product p, double amount){
