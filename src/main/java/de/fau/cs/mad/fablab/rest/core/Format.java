18b543a0b9bcd198e88f961dd4d9c60e84775232
Philip Kanhaeusser <git@mail.philip.xers.de>
2015-05-28 11:59:46 +0200

    Added Date format string to common repo

---
+++ src/main/java/de/fau/cs/mad/fablab/rest/core/Format.java
@@ -0,0 +1,8 @@
+package de.fau.cs.mad.fablab.rest.core;
+
+/**
+ * Some shared format strings for client and server
+ */
+public class Format {
+    public static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZ";
+}
