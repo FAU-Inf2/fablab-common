ad61ef5d3ece6fb1a110c5722766abd816ad621d
Sebastian Haubner <sebastian.haubner@fau.de>
2015-06-01 13:41:54 +0200

    Change name for paid status in CartStatusEnum

--- src/main/java/de/fau/cs/mad/fablab/rest/core/CartStatusEnum.java
+++ src/main/java/de/fau/cs/mad/fablab/rest/core/CartStatusEnum.java
@@ -4,5 +4,5 @@ package de.fau.cs.mad.fablab.rest.core;
  * Created by EE on 12.05.15.
  */
 public enum CartStatusEnum {
-    SHOPPING, PENDING, FINISHED, CANCELLED
+    SHOPPING, PENDING, PAID, CANCELLED
 }
