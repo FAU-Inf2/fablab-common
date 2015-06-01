b2bd0d47600a5c81f6dab46b06230338f65db71c
Sebastian Haubner <sebastian.haubner@fau.de>
2015-06-01 12:51:32 +0200

    change type of cart id to string

--- src/main/java/de/fau/cs/mad/fablab/rest/core/Cart.java
+++ src/main/java/de/fau/cs/mad/fablab/rest/core/Cart.java
@@ -16,7 +16,7 @@ public class Cart implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private String id;
 
     @Column(name = "status")
     private CartStatusEnum status;
@@ -38,10 +38,10 @@ public class Cart implements Serializable {
 
 
     @JsonProperty
-    public long getId() {
+    public String getId() {
         return id;
     }
-    public void setId(long id) {
+    public void setId(String id) {
         this.id = id;
     }
 
