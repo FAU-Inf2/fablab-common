bf788d18038e2a24921c5c79547abd3d39ad9d05
Johannes Pfann <jopf88@googlemail.com>
2015-06-01 10:38:56 +0200

    - Modified PushAPI with @Post for @Get Annotation und returnvalue Response

--- src/main/java/de/fau/cs/mad/fablab/rest/api/PushApi.java
+++ src/main/java/de/fau/cs/mad/fablab/rest/api/PushApi.java
@@ -1,8 +1,6 @@
 package de.fau.cs.mad.fablab.rest.api;
 
 
-import de.fau.cs.mad.fablab.rest.entities.WelcomeUser;
-import io.dropwizard.jersey.params.LongParam;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -11,11 +9,11 @@ import javax.ws.rs.core.Response;
 @Path("/push")
 public interface PushApi {
 
-    @GET
+    @POST
     @Path("/{regid}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    WelcomeUser addRegistrationId(@PathParam("regid") String regId);
+    Response addRegistrationId(@PathParam("regid") String regId);
 
 }
 
