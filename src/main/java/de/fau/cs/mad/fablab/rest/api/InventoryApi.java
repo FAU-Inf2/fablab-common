86ad4d5c97f34842dc4f8e41b20416f4fe502f50
Julia Schottenhamml <julia.schottenhamml@medtech.stud.uni-erlangen.de>
2015-08-20 16:06:51 +0200

    changed datatype so that fablab-android builds again

--- src/main/java/de/fau/cs/mad/fablab/rest/api/InventoryApi.java
+++ src/main/java/de/fau/cs/mad/fablab/rest/api/InventoryApi.java
@@ -1,15 +1,19 @@
 package de.fau.cs.mad.fablab.rest.api;
 
-import de.fau.cs.mad.fablab.rest.core.CartStatus;
-import de.fau.cs.mad.fablab.rest.core.InventoryItem;
-
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
 import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import de.fau.cs.mad.fablab.rest.core.InventoryItem;
 
 @Path("inventory")
 public interface InventoryApi {
-
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
@@ -21,7 +25,7 @@ public interface InventoryApi {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/deleteAll")
-    boolean deleteAll();
+    Boolean deleteAll();
 
     @GET
     @Consumes(MediaType.APPLICATION_JSON)
