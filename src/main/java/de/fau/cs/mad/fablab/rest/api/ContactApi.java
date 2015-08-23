63ae90c473f6ba191a03addbce7882d8d96e3312
Johannes Pfann <jopf88@googlemail.com>
2015-08-23 14:14:00 +0200

    - Modified ContactAPI with some testapis - Added TestObject to test post-request

--- src/main/java/de/fau/cs/mad/fablab/rest/api/ContactApi.java
+++ src/main/java/de/fau/cs/mad/fablab/rest/api/ContactApi.java
@@ -1,27 +1,46 @@
 package de.fau.cs.mad.fablab.rest.api;
 
 
+import com.codahale.metrics.annotation.ExceptionMetered;
+import com.codahale.metrics.annotation.Metered;
+import com.codahale.metrics.annotation.Timed;
+import de.fau.cs.mad.fablab.rest.core.CartServer;
+import de.fau.cs.mad.fablab.rest.core.Product;
 import de.fau.cs.mad.fablab.rest.core.RegistrationId;
+import de.fau.cs.mad.fablab.rest.core.TestObject;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("mail")
 public interface ContactApi {
 
-    @PUT
+    @POST
+    @Path("/feedback")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     Response sendFeedback(String aMessage);
 
-
-    @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
+    @POST
+    @Path("/error")
     @Produces(MediaType.APPLICATION_JSON)
     Response sendErrorMessage(long aToolId);
 
+    @POST
+    @Path("/test")
+    @Metered
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    TestObject create(final TestObject obj);
+
+    @GET
+    @Path("/cartserver")
+    @Produces(MediaType.APPLICATION_JSON)
+    CartServer getCartServer();
+
+    @GET
+    @Path("/test2")
+    @Produces(MediaType.APPLICATION_JSON)
+    TestObject getTestObject();
 }
