b0e0401604677bd7b7df37a0a8a74ab6422f5637
Emanuel Eimer <mail@eeimer.de>
2015-08-27 18:52:55 +0200

    stared with persistence of PushToken

--- src/main/java/de/fau/cs/mad/fablab/rest/core/PushToken.java
+++ src/main/java/de/fau/cs/mad/fablab/rest/core/PushToken.java
@@ -1,21 +1,42 @@
 package de.fau.cs.mad.fablab.rest.core;
 
 
-import de.fau.cs.mad.fablab.rest.api.PlatformType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
-
+@Entity
+@Table(name = "push_token")
 public class PushToken implements Serializable{
 
 
+    @Id
+    @JsonIgnore
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "database_id")
+    private long DatabaseId;
 
-    protected String token;
+    @Column(name = "token")
+    private String token;
 
-    protected PlatformType platformType = PlatformType.UNSPECIFIED;
+    @Column(name = "platform_type")
+    private PlatformType platformType = PlatformType.UNSPECIFIED;
+
+
+    @JsonIgnore
+    @Column(name = "trigger_push_type")
+    private TriggerPushType triggerPushType;
 
     public PushToken(){}
 
+    public long getDatabaseId() {
+        return DatabaseId;
+    }
+
+    public void setDatabaseId(long databaseId) {
+        DatabaseId = databaseId;
+    }
 
     public String getToken() {
         return token;
@@ -31,5 +52,14 @@ public class PushToken implements Serializable{
 
     public void setPlatformType(PlatformType platformType) {
         this.platformType = platformType;
+    }
+
+
+    public TriggerPushType getTriggerPushType() {
+        return triggerPushType;
+    }
+
+    public void setTriggerPushType(TriggerPushType triggerPushType) {
+        this.triggerPushType = triggerPushType;
     }
 }
